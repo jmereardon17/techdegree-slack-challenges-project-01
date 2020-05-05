bcefbe9aa05924509143258f1c8e96b2c3c1634e
Jamie Reardon <decor1878@gmail.com>
2020-05-05 12:22:01 +0100

    Project starter files

---
+++ js/app.js
@@ -0,0 +1,30 @@
+/* *****************************************************
+  Variables
+***************************************************** */
+// Alert Notification Banner
+const alert = document.getElementById('notification');
+const closeIcon = document.querySelector('.close-icon');
+// Projects list UL
+const projectList = document.getElementById('projectList');
+
+// Project form elements
+const projectNameInput = document.getElementById('projectName');
+const projectHTMLCheckbox = document.getElementById('html');
+const projectCSSCheckbox = document.getElementById('css');
+const projectJSCheckbox = document.getElementById('js');
+const projectDetailsInput = document.getElementById('projectDetails');
+const AddProjectButton = document.getElementById('addProjectButton');
+
+// Student Invite form elements
+const studentNameInput = document.getElementById('studentName');
+const studentProjectSelect = document.getElementById('projectSelect');
+const inviteStudentButton = document.getElementById('inviteStudentButton');
+
+// Event Listeners
+addProjectButton.addEventListener('click', e => {});
+
+projectList.addEventListener('click', e => {});
+
+inviteStudentButton.addEventListener('click', e => {});
+
+closeIcon.addEventListener('click', () => {});
